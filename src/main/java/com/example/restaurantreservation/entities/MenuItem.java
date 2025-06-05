dee305495e42af130d15abd50f392f265455bade
Karan Paul <134717527+workwithkp96@users.noreply.github.com>
2025-06-05 17:03:39 +0530

    Create MenuItem entity

---
+++ src/main/java/com/example/restaurantreservation/entities/MenuItem.java
@@ -0,0 +1,19 @@
+package com.example.restaurantreservation.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+
+    // Getters and Setters
+}
